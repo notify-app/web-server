cbcbae10aaecd5eb8243b5a3243a7535aa5e2dff
Luca Tabone <taboneluca3@gmail.com>
2017-01-29 12:45:21 +0100

    Fix issue with direct links

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const config = require('./config')
 const app = express()
 
 app.use('/login', express.static(path.join(__dirname, './public/login')))
-app.use('/', express.static(path.join(__dirname, './public/app')))
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'))
+})
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`))
